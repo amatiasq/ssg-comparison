f51f426639e30c69142b40dc7b07c47f9abd0fb5
A. Matías Quezada <amatiasq@gmail.com>
2021-08-26 18:27:27 +0200

    Cleanup

--- gridsome/src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1629470362944
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'amatiasq.com',
+    title: 'NuxtJS',
     htmlAttrs: {
       lang: 'en',
     },
